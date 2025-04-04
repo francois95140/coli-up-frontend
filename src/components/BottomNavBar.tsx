38a33ded0194f62e7cbda92e1723417a96bc5b5b
AITBOUKHA Elias <elias.aitboukha@gmail.com>
2025-04-04 13:47:55 +0200

    commit navbar

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import boxSvg from "../assets/images/boxicon.svg";
+import "/src/assets/css/navbar.css";
 
 const BottomNavBar: React.FC = () => {
   const location = useLocation();
@@ -9,38 +11,33 @@ const BottomNavBar: React.FC = () => {
   };
 
   return (
-    <nav className="bottom-nav">
-      <Link to="/home" className={`nav-item ${isActive('/home')}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-          <polyline points="9 22 9 12 15 12 15 22"></polyline>
-        </svg>
-        Accueil
-      </Link>
-      <Link to="/packages" className={`nav-item ${isActive('/packages')}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line>
-          <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-          <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
-          <line x1="12" y1="22.08" x2="12" y2="12"></line>
-        </svg>
-        Colis
-      </Link>
-      <Link to="/notifications" className={`nav-item ${isActive('/notifications')}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-        </svg>
-        Alertes
-      </Link>
-      <Link to="/profile" className={`nav-item ${isActive('/profile')}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-          <circle cx="12" cy="7" r="4"></circle>
-        </svg>
-        Profil
-      </Link>
-    </nav>
+
+    
+    <section id="navbar" className="fixed bottom-0 w-full py-3 h-[4rem] max-h-[4rem] bg-gray-200">
+        <div className="flex items-center w-full h-full justify-evenly nav">
+            <button className="flex justify-center items-center active">
+                <ion-icon name="settings-outline" className="text-3xl"></ion-icon>
+            </button>
+            <button className="flex justify-center items-center">
+                <ion-icon name="time-outline" className="text-3xl"></ion-icon>
+            </button>
+            <button className="flex justify-center items-center">
+                <ion-icon name="home-outline" className="text-3xl"></ion-icon>
+            </button>
+            <button className="flex justify-center items-center">
+              <span class="text-3xl  size-3xl">
+                <img src={boxSvg} alt="Box Icon" className="h-full w-full" />
+              </span>
+            </button>
+            <button className="flex justify-center items-center">
+                <ion-icon name="people-outline" className="text-3xl"></ion-icon>
+            </button>
+            <div className="selector size-3xl rounded-full fixed">
+
+            </div>
+        </div>
+        
+    </section>
   );
 };
 
